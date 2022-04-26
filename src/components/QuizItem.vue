011aeea6e5e45c6dfb485179fd291b3651585507
Sanket Patel <sanketsohaliya@gmail.com>
2022-04-26 17:28:13 +0530

    Updated

--- src/components/QuizItem.vue
+++ src/components/QuizItem.vue
@@ -1,20 +1,17 @@
 <template>
 	<main>
 		<section class="p-3 m-6 rounded-md bg-white" v-if="!quizStore.quizCompleted">
-			<div class="flex justify-between mb-7">
-				<span>{{ quizStore.title }}</span>
-				<span>{{ $route.params.id }}</span>
-				<span class="question">{{ quizStore.getCurrentQuestion.question }}</span>
-				<span class="score">Score {{ quizStore.score }}/{{ quizStore.questions.length }}</span>
+			<div class="text-center">
+				<h1>{{ quizStore.getCurrentQuestion.question }}</h1>
 			</div>
 
 			<div class="mb-7">
 				<label v-for="(option, index) in quizStore.getCurrentQuestion.options" :for="'option' + index"
 					:key="index" :class="`m-3 p-3 block bg-blue-200 rounded-md cursor-pointer ${quizStore.getCurrentQuestion.selected == index
-						? index == quizStore.getCurrentQuestion.answer
-							? 'bg-green-700 text-white'
-							: 'bg-red-400 text-white'
-						: ''
+					? index == quizStore.getCurrentQuestion.answer
+						? 'bg-green-700 text-white'
+						: 'bg-red-400 text-white'
+					: ''
 					} ${quizStore.getCurrentQuestion.selected != null &&
 						index != quizStore.getCurrentQuestion.selected
 						? 'opacity-50'
@@ -22,14 +19,16 @@
 					}`">
 					<input type="radio" :id="'option' + index" :name="quizStore.getCurrentQuestion.index" :value="index"
 						v-model="quizStore.getCurrentQuestion.selected"
-						:disabled="quizStore.getCurrentQuestion.selected" @change="quizStore.setAnswer" class="hidden"/>
+						:disabled="quizStore.getCurrentQuestion.selected" @change="quizStore.setAnswer"
+						class="hidden" />
 					<span>{{ option }}</span>
 				</label>
 			</div>
 
-			<button @click="quizStore.nextQuestion" :disabled="!quizStore.getCurrentQuestion.selected" class="px-7 py-3 text-xl font-semibold bg-yellow-200 text-slate-800 rounded-md cursor-pointer disabled:opacity-50">
+			<button @click="quizStore.nextQuestion" :disabled="!quizStore.getCurrentQuestion.selected"
+				class="px-7 py-3 text-xl font-semibold bg-yellow-200 text-slate-800 rounded-md cursor-pointer disabled:opacity-50">
 				{{
-						quizStore.getCurrentQuestion.index == quizStore.questions.length - 1
+						quizStore.getCurrentQuestion.index == quizStore.quiz.questions.length - 1
 							? 'Finish'
 							: quizStore.getCurrentQuestion.selected == null
 								? 'Select an option'
@@ -40,15 +39,17 @@
 
 		<section v-else>
 			<h2>You have finished the quiz!</h2>
-			<p>Your score is {{ quizStore.score }}/{{ quizStore.questions.length }}</p>
+			<p>Your score is {{ quizStore.score }}/{{ quizStore.quiz.questions.length }}</p>
 		</section>
 	</main>
 </template>
 
 <script setup>
 import { useQuizStore } from '@/stores/quiz'
+import { useRoute } from "vue-router";
+const route = useRoute();
+const id = route.params.id;
 const quizStore = useQuizStore()
 quizStore.getUser();
-quizStore.getQuiz();
-quizStore.getQuestions();
+quizStore.getQuiz(id);
 </script>
--- src/stores/quiz.js
+++ src/stores/quiz.js
@@ -10,7 +10,7 @@ export const useQuizStore = defineStore({
         doubleCount: (state) => state.counter * 2,
         score: (state) => {
             let value = 0
-            state.questions.map(q => {
+            state.quiz.questions.map(q => {
                 if (q.selected != null && q.answer == q.selected) {
                     console.log('correct');
                     value++
@@ -19,7 +19,7 @@ export const useQuizStore = defineStore({
             return value
         },
         getCurrentQuestion: (state) => {
-            let question = state.questions[state.currentQuestion]
+            let question = state.quiz.questions[state.currentQuestion]
             question.index = state.currentQuestion
             return question
         }
@@ -30,49 +30,77 @@ export const useQuizStore = defineStore({
                 .then(response => response.json())
                 .then(json => this.userData = json)
         },
-        getQuiz() {
-            this.title = 'Quiz Title'
-        },
-        getQuestions() {
-            this.questions = [
-                {
-                    question: 'What is Vue?',
-                    answer: 0,
-                    options: [
-                        'A framework',
-                        'A library',
-                        'A type of hat'
-                    ],
-                    selected: null
-                },
-                {
-                    question: 'What is Vuex used for?',
-                    answer: 2,
-                    options: [
-                        'Eating a delicious snack',
-                        'Viewing things',
-                        'State management'
-                    ],
-                    selected: null
-                },
-                {
-                    question: 'What is Vue Router?',
-                    answer: 1,
-                    options: [
-                        'An ice cream maker',
-                        'A routing library for Vue',
-                        'Burger sauce'
-                    ],
-                    selected: null
-                }
-            ]
+        getQuiz(id) {
+            this.quiz = {
+                id,
+                title: 'How Much Do You Know About New York City History?',
+                image: 'https://files.brainfall.com/wp-content/uploads/2022/03/how_much_about_the_history_of_new_york_city_do_you_know-936x0.jpg.webp',
+                questions: [
+                    {
+                        question: 'In the Revolutionary War, which was a battle George Washington won in New York City?',
+                        answer: 1,
+                        options: [
+                            'Battle of the Bulge',
+                            'Battle of Harlem Heights',
+                            'Battle of Prospect Park',
+                            'The Skirmish in Bed-Stuy'
+                        ],
+                        selected: null
+                    },
+                    {
+                        question: 'Under the Articles of Confederation, what distinction did New York City carry?',
+                        answer: 1,
+                        options: [
+                            'It was the only place where imports were allowed to come into the nation',
+                            'It was the national capital',
+                            'It was where all the delegates needed to live',
+                            'The nation was named New York Nation after it'
+                        ],
+                        selected: null
+                    },
+                    {
+                        question: 'The nation was named New York Nation after it',
+                        answer: 1,
+                        options: [
+                            'Murray Hill, Tribeca, Chelsea, Midtown East, Kew Gardens',
+                            'Staten Island, Manhattan, Queens, Bronx, Brooklyn',
+                            'Staten Island, Long Island, Queens, Manhattan, Brooklyn',
+                            'Houston, Mott, West 4th, Avenue A, River'
+                        ],
+                        selected: null
+                    },
+                    {
+                        question: 'Who did Henry Hudson work for?',
+                        answer: 3,
+                        options: [
+                            'The Worldwide Financial Industry',
+                            'The Municipal Assistance Corporation',
+                            'The Atlantic Port',
+                            'The Dutch East India Company',
+                        ],
+                        selected: null
+                    },
+                    {
+                        question: 'What was the name of the Dutch settlement at the southern tip of Manhattan Island that defended the fur trade operations for the Dutch West India Company?',
+                        answer: 2,
+                        options: [
+                            'New York',
+                            'New Your Bay',
+                            'New Amsterdam',
+                            'Lower Manhattan',
+                        ],
+                        selected: null
+                    }
+                ]
+            }
+
         },
         setAnswer(e) {
-            this.questions[this.currentQuestion].selected = e.target.value
+            this.quiz.questions[this.currentQuestion].selected = e.target.value
             e.target.value = null
         },
         nextQuestion() {
-            if (this.currentQuestion < this.questions.length - 1) {
+            if (this.currentQuestion < this.quiz.questions.length - 1) {
                 this.currentQuestion++
                 return
             }
