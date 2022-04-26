9d51f8f2d79fdd1e510cb0061606353e881e5bfc
Sanket Patel <sanketsohaliya@gmail.com>
2022-04-26 18:16:35 +0530

    Updated

--- src/components/QuizThumbnails.vue
+++ src/components/QuizThumbnails.vue
@@ -2,12 +2,12 @@
     <section class="text-gray-600">
         <div class="container mx-auto">
             <div class="flex flex-wrap mx-3">
-                <QuizThumbnail id="1"/>
-                <QuizThumbnail id="2"/>
-                <QuizThumbnail id="3"/>
-                <QuizThumbnail id="4"/>
-                <QuizThumbnail id="5"/>
-                <QuizThumbnail id="6"/>
+                <QuizThumbnail />
+                <QuizThumbnail />
+                <QuizThumbnail />
+                <QuizThumbnail />
+                <QuizThumbnail />
+                <QuizThumbnail />
             </div>
         </div>
     </section>
