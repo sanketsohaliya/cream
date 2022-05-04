b88802dc79cb9710c3a844648a8bf96d3281e712
Sanket Patel <sanketsohaliya@gmail.com>
2022-05-04 23:47:52 +0530

    Updated

--- src/components/QuizThumbnails.vue
+++ src/components/QuizThumbnails.vue
@@ -1,18 +1,25 @@
 <template>
     <section>
-        <div class="flex flex-wrap mx-3">
-            <QuizThumbnail :thumbnailId="0" />
-            <QuizThumbnail :thumbnailId="1" />
-            <QuizThumbnail :thumbnailId="2" />
-            <QuizThumbnail :thumbnailId="3" />
-            <QuizThumbnail :thumbnailId="4" />
-            <QuizThumbnail :thumbnailId="5" />
-            <QuizThumbnail :thumbnailId="6" />
-            <QuizThumbnail :thumbnailId="7" />
-            <QuizThumbnail :thumbnailId="8" />
+        <div v-if="!quizStore.isEmpty" class="flex flex-wrap mx-3">
+            <div v-for="(quiz, id) in quizStore.quizzes" :key="id" class="p-5 md:w-1/3">
+                <router-link @click="quizStore.getQuiz(id)" :to="{
+                    name: 'quiz.show',
+                    params: { id: id }
+                }">
+                    <div class="h-full rounded-xl shadow-cla-blue bg-white overflow-hidden">
+                        <img class="h-48 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100"
+                            :src="quiz.image" alt="Quiz Thumbnail">
+                        <div class="p-5">
+                            <div class="tracking-widest text-sm text-gray-500 mb-1">{{ quiz.category }}</div>
+                            <div class="leading-relaxed text-lg mb-3">{{ quiz.title }}</div>
+                        </div>
+                    </div>
+                </router-link>
+            </div>
         </div>
     </section>
 </template>
 <script setup>
-import QuizThumbnail from '@/components/QuizThumbnail.vue'
+import { useQuizStore } from '@/stores/quiz'
+const quizStore = useQuizStore()
 </script>
