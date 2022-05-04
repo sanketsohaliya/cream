1ffe1f36d5ac2cb87e84043456fff838b5e45d59
Sanket Patel <sanketsohaliya@gmail.com>
2022-05-04 19:40:44 +0530

    Updated

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('../views/HomeView.vue')
+  },
+  {
+    path: '/quiz/:id',
+    name: 'quiz',
+    component: () => import('../views/QuizView.vue'),
+    props: true
+  }
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
   scrollBehavior() {
-    // always scroll to top
     return { top: 0 }
-  },
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/HomeView.vue')
-    },
-    {
-      path: '/quiz/:id',
-      name: 'quiz',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/QuizView.vue'),
-      props: true
-    }
-  ]
+  }
 })
 
 export default router
