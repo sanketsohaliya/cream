29638cc8c1a86401923abf215fda168376b2cef2
Sanket Patel <sanketsohaliya@gmail.com>
2022-05-04 21:03:42 +0530

    Updated

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
 
 const routes = [
-  { path: '/', name: 'home', component: () => import('@/views/HomeView.vue') },
+  { path: '/', name: 'home', component: HomeView },
   { path: '/quiz/:id', name: 'quiz', component: () => import('@/views/QuizView.vue'), props: true }
 ]
 
